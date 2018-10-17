af690b25853eeb2f1bce8d2bdbb39e581ce5829b
lawofgamedesign <tom@lawofgamedesign.com>
2018-10-17 14:19:29 -0400

    Added placeholder title scene
    Game now loads to placeholder title scene, which loads game on any touch input.

---
+++ FlightnoFight/Assets/Scripts/Title/TitleManager.cs
@@ -0,0 +1,25 @@
+﻿namespace Title
+{
+    using UnityEngine;
+    using UnityEngine.SceneManagement;
+
+    public class TitleManager : MonoBehaviour
+    {
+
+
+        ////////////////////////////////////////////////
+        /// Fields
+        ////////////////////////////////////////////////
+
+
+        //scene to load
+        private const string GAME_SCENE = "Game";
+
+
+        //load game on touch
+        void Update()
+        {
+            if (Input.touchCount > 0) SceneManager.LoadScene(GAME_SCENE);
+        }
+    }
+}
